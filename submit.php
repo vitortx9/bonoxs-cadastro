be710e197438f46ae1bc6d98e2fee16f26a58b71
vitortx9 <tx9vitor@gmail.com>
2024-07-23 13:49:39 -0300

    Update submit.php

--- submit.php
+++ submit.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "top_secret";
+$dbname = "bonoxs_cadastro";
 
 // Cria conexão com o banco de dados
 $conn = new mysqli($servername, $username, $password, $dbname);
